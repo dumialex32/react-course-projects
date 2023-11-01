14e6354e1cc865944d9a0e7eee2d2f3e443f50de
dumialex32 <dumitrasalexandru26@gmail.com>
2023-11-01 13:21:11 +0100

    Second commit

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,14 @@
 import DateCounter from "./DateCounter";
 import { useState, useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
 export default function App() {
   return (
     <div className="app">
-      <DateCounter />
+      <Header />
+
+      <Main></Main>
     </div>
   );
 }
